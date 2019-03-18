d383ef18f2663b208e67c1b812c3e1ebdbb2ccd7
alexjoffroy <alexandre.joffroy@gmail.com>
2019-03-18 14:50:46 +0100

    Generate command - Rework FS reading to fix Netlify deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -103,26 +103,24 @@ module.exports = {
   generate: {
     subFolders: true,
     routes() {
-      return fs
-        .readdirSync(`contents/blog-posts`)
-        .map(locale => {
-          return fs
-            .readdirSync(`contents/blog-posts/${locale}`)
-            .map(filename => {
-              let [_, publishedAt, slug] = filename.match(
-                /(\d{4}-\d{2}-\d{2}).(.+)\.md$/
-              )
+      let routes = ['/', '/blog', '/contact', '/contact/success']
 
-              return {
-                route:
-                  locale != 'en'
-                    ? `/${locale}/blog/${slug}-${+new Date(publishedAt) / 1000}`
-                    : `/blog/${slug}-${+new Date(publishedAt) / 1000}`
-              }
-            })
+      fs.readdirSync(`contents/blog-posts`).map(locale => {
+        fs.readdirSync(`contents/blog-posts/${locale}`).map(filename => {
+          let [_, publishedAt, slug] = filename.match(
+            /(\d{4}-\d{2}-\d{2}).(.+)\.md$/
+          )
+
+          routes.push({
+            route:
+              locale != 'en'
+                ? `/${locale}/blog/${slug}-${+new Date(publishedAt) / 1000}`
+                : `/blog/${slug}-${+new Date(publishedAt) / 1000}`
+          })
         })
-        .flat()
-        .concat(['/', '/blog', '/contact', '/contact/success'])
+      })
+
+      return routes
     }
   },
 
