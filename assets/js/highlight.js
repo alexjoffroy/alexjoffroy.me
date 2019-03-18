acd58017c85ec59199eb866c1d67d9b755c3b0b9
alexjoffroy <alexandre.joffroy@gmail.com>
2019-03-18 09:51:01 +0100

    Add Highlight.js to RenderFrontMatter component

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,4 +1,3 @@
-<script>
 const hljs = require('highlight.js/lib/highlight')
 
 hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
@@ -12,31 +11,8 @@ hljs.registerLanguage('php', require('highlight.js/lib/languages/php'))
 hljs.registerLanguage('scss', require('highlight.js/lib/languages/scss'))
 hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
 
-export default {
-  props: {
-    source: {
-      type: Object,
-      required: true
-    }
-  },
-  data() {
-    return {
-      templateRender: false
-    }
-  },
-  created() {
-    this.templateRender = new Function(this.source.vue.render)()
-    this.$options.staticRenderFns = new Function(
-      this.source.vue.staticRenderFns
-    )()
-  },
-  mounted() {
-    document.querySelectorAll('pre code').forEach(codeblock => {
-      hljs.highlightBlock(codeblock)
-    })
-  },
-  render(h) {
-    return this.templateRender ? this.templateRender() : h('div', 'Rendering')
-  }
+export function start() {
+  document.querySelectorAll('pre code').forEach(codeblock => {
+    hljs.highlightBlock(codeblock)
+  })
 }
-</script>
