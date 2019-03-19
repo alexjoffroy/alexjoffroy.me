92dab9405549e475e5dd5682bee867cfa6484760
alexjoffroy <alexandre.joffroy@gmail.com>
2019-03-19 12:18:03 +0100

    Redirect / to /blog

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <script>
+// Redirects are already made "server-side" (see netlify.toml), but it's needed to do it client side too (for SPA mode)
 export default {
   render() {
     return this.$router.replace(this.localePath({ name: 'blog' }))
