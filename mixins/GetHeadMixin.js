fd2e4e38d7c59786c11e84d5e3c17c7b13e37ad0
alexjoffroy <alexandre.joffroy@gmail.com>
2019-03-20 16:10:13 +0100

    Add og:image and twitter:image metas

--- mixins/GetHeadMixin.js
+++ mixins/GetHeadMixin.js
@@ -1,3 +1,6 @@
+const image =
+  'https://avatars2.githubusercontent.com/u/7209163?s=400&u=a0fc548942af6abcefc2b93e00285752eef2d6b0&v=4'
+
 export default {
   methods: {
     getHead({ title, description }) {
@@ -21,11 +24,17 @@ export default {
             property: 'og:url',
             content: process.env.baseUrl + this.$route.path
           },
+          { hid: 'og:image', property: 'og:image', content: image },
           { hid: 'twitter:title', name: 'twitter:title', content: title },
           {
             hid: 'twitter:description',
             name: 'twitter:description',
             content: description
+          },
+          {
+            hid: 'twitter:image',
+            name: 'twitter:image',
+            content: image
           }
         ]
       }
