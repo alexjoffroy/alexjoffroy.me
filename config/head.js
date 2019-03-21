cb4fe27637a17965a3fa222676175369113978cd
alexjoffroy <alexandre.joffroy@gmail.com>
2019-03-21 16:39:20 +0100

    Add preconnect link for https://fonts.gstatic.com/

--- config/head.js
+++ config/head.js
@@ -4,6 +4,10 @@ export default {
   },
   link: [
     {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com/'
+    },
+    {
       rel: 'stylesheet',
       href:
         'https://fonts.googleapis.com/css?family=Quicksand:300,400,700|Source:300,400,700'
