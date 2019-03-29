ee2f76d0a336e4ed64179a15d66020fbcf01538b
alexjoffroy <alexandre.joffroy@gmail.com>
2019-03-29 17:36:10 +0100

    Update contact page layout

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,53 +1,51 @@
 <template>
   <div>    
-    <main class="mb-16">
-
-      <form 
-        :action="localePath({ name: 'contact-success' })"
-        name="contact" 
-        method="POST"
-        data-netlify-recaptcha="true"
-        data-netlify="true">
+    <h1 class="text-3xl text-grey-darkest font-bold font-heading mb-16">{{ $t('contact.title') }}</h1>
+    <form 
+      :action="localePath({ name: 'contact-success' })"
+      class="mb-8"
+      name="contact" 
+      method="POST"
+      data-netlify-recaptcha="true"
+      data-netlify="true">
         
-        <input 
-          type="hidden" 
-          name="form-name" 
-          value="contact">
+      <input 
+        type="hidden" 
+        name="form-name" 
+        value="contact">
         
-        <form-field
-          :required="true"
-          :label="$t('contact.email')"
-          :placeholder="$t('contact.email_placeholder')"
-          class="mb-8 md:w-1/2 lg-1/3"
-          name="email"
-          type="email" />
+      <form-field
+        :required="true"
+        :label="$t('contact.email')"
+        :placeholder="$t('contact.email_placeholder')"
+        class="mb-8 md:w-1/2 lg-1/3"
+        name="email"
+        type="email" />
 
-        <form-field
-          :required="true"
-          :label="$t('contact.subject')"
-          :placeholder="$t('contact.subject_placeholder')"
-          class="mb-8 md:w-1/2"
-          name="subject" />
+      <form-field
+        :required="true"
+        :label="$t('contact.subject')"
+        :placeholder="$t('contact.subject_placeholder')"
+        class="mb-8 md:w-1/2"
+        name="subject" />
 
-        <form-field
-          :required="true"
-          :label="$t('contact.content')"
-          :placeholder="$t('contact.content_placeholder')"
-          class="mb-8"
-          name="content"
-          type="textarea" />               
+      <form-field
+        :required="true"
+        :label="$t('contact.content')"
+        :placeholder="$t('contact.content_placeholder')"
+        class="mb-8"
+        name="content"
+        type="textarea" />               
 
-        <div 
-          :data-sitekey="recaptchaKey" 
-          class="g-recaptcha mb-8"/>
+      <div 
+        :data-sitekey="recaptchaKey" 
+        class="g-recaptcha mb-8"/>
 
-        <button 
-          class="button is-black has-arrow mx-auto" 
-          type="submit">{{ $t('contact.send') }}</button>
+      <button 
+        class="button is-black has-arrow mx-auto" 
+        type="submit">{{ $t('contact.send') }}</button>
 
-      </form>
-        
-    </main>
+    </form>
   </div>
 </template>
 
